552a6585de62b3f4d0367a080f11716dc3e306b3
Kuan314 <limzhengkuan@hotmail.com>
2021-09-25 17:17:53 +0800

    add some function

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 function changeName() {
     document.getElementById("name").innerHTML = "I'm Lim Zheng Kuan";
+
+    // if (document.getElementById("name").innerHTML == "I'm Lim Zheng Kuan") { }
 }
