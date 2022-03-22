cc465f3ec0db6a5422a320010bda077d16524879
Kuan314 <limzhengkuan@hotmail.com>
2022-03-22 23:06:33 +0800

    restart

--- main.js
+++
@@ -1,5 +0,0 @@
-function changeName() {
-    document.getElementById("name").innerHTML = "I'm Lim Zheng Kuan";
-
-    // if (document.getElementById("name").innerHTML == "I'm Lim Zheng Kuan") { }
-}
